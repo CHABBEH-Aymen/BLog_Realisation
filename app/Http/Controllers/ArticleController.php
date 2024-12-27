7cb8c77cbc9842c7a80b58fbf6be95fa0f4f0561
CHABBEH-Aymen <chabbehaymensolicode@gmail.com>
2024-12-27 17:13:43 +0100

    Basic view and some progress in crud (#26)
    * Create Laravel Models (Category, Comment) (#11)
    
    * tag (#12)
    
    Co-authored-by: Ayoub-El-Bouzidi <elbouzidiayoub.solicode.@gmail.com>
    
    * Feature : Create functionality for all models (#15)
    
    * completed create for article
    
    * completed create for article
    
    * completed creations
    
    ---------
    
    Co-authored-by: ChabbehAymen <devaymen15.5.3@gmail.com>
    
    * conception
    
    * delete (#23)
    
    Co-authored-by: Ayoub-El-Bouzidi <elbouzidiayoub.solicode.@gmail.com>
    
    * permission_user (#22)
    
    * permission_user
    
    * changes in user_permission
    
    ---------
    
    Co-authored-by: CHABBEH-Aymen <chabbehaymensolicode@gmail.com>
    
    * completed update
    
    * Article view (#24)
    
    * delete
    
    * article_view
    
    ---------
    
    Co-authored-by: Ayoub-El-Bouzidi <elbouzidiayoub.solicode.@gmail.com>
    Co-authored-by: CHABBEH-Aymen <chabbehaymensolicode@gmail.com>
    
    * test
    
    * t
    
    * t
    
    * 7 update (#25)
    
    * completed update
    
    * test
    
    * t
    
    ---------
    
    Co-authored-by: ChabbehAymen <devaymen15.5.3@gmail.com>
    
    * installed adminlte
    
    * fixed error
    
    * fixed auth error
    
    ---------
    
    Co-authored-by: Oussama Bardich <98558799+bardichoussama@users.noreply.github.com>
    Co-authored-by: Ayoub-El-Bouzidi <153141906+Ayoub-El-Bouzidi@users.noreply.github.com>
    Co-authored-by: Ayoub-El-Bouzidi <elbouzidiayoub.solicode.@gmail.com>
    Co-authored-by: ChabbehAymen <devaymen15.5.3@gmail.com>
    Co-authored-by: ZOUBAIRDouae <zoubairdouae5@gmail.com>

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Creating a new resource.
+     */
+    public function create()
+    {
+        return view('article.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        return view('admin.article.create');
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $article = Article::findOrFail($id);
+        $data = $request->validate([
+            "title"=>"required|string|max:255",
+            "content"=>"required|string",
+        ]); 
+        $article->title = $data["title"];
+        $article->content = $data["content"];
+        return redirect()->back();
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- database/migrations/2024_12_25_115825_create_articles_table.php
+++ database/migrations/2024_12_25_115825_create_articles_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("user_id")->constrained()->onDelete("cascade");
-            $table->string("title");
-            $table->longText("txt");
+            $table->string('title');
+            $table->text('content');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
