f02ecd78fa70c915edcc3679cbcd2e88a75f73bd
ChabbehAymen <devaymen15.5.3@gmail.com>
2024-12-28 13:47:08 +0100

    permissions

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,16 +16,28 @@ Route::middleware("auth")->group(function () {
     Route::get("dashboard", function () {
         return view('dashboard');
     })->name("dashboard");
+    // comment routes
+    Route::get("/comment/create", [CommentController::class, "create"])->name("comment.create");
+    Route::post("/comment/store", [CommentController::class, "store"])->name("comment.store");
+    // only user that owns the comment and the admin can delete comment
+    // registred in CommentPermissions Middleware
+    Route::delete("/comment/delete", [CommentController::class, "delete"])
+    ->name("comment.delete")
+    ->middleware("auth.comment");
+    
+    // only admin should create articles
+    //create
+    Route::get('/articles/create', [ArticleController::class, 'create'])
+    ->name('article.create')
+    ->middleware("role:admin");
+    //store
+    Route::post('/articles/store', [ArticleController::class, 'store'])
+    ->name('article.store')
+    ->middleware("role:admin");
 });
 
-// create article
-Route::get('/articles/create', [ArticleController::class, 'create'])->name('article.create');
+// any one can see comments this should be public
+Route::get('/comments' , [App\Http\Controllers\CommentController::class, 'index'])->name('comments');
 
 
-Route::middleware(['permission:add comment'])->group(function() {
-    Route::get('/comments' , [App\Http\Controllers\CommentController::class, 'index'])->name('comments');
-});
-
-//public
-// Route::view('/public', 'public.home');
 
