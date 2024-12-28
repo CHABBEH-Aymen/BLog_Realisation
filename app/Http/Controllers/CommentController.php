bdd54f967de7c905adfc1679029c2f33da2d8500
CHABBEH-Aymen <chabbehaymensolicode@gmail.com>
2024-12-28 14:17:31 +0100

    Permission user (#32)
    * permission_user
    
    * changes in user_permission
    
    * permissions
    
    ---------
    
    Co-authored-by: ZOUBAIRDouae <zoubairdouae5@gmail.com>
    Co-authored-by: ChabbehAymen <devaymen15.5.3@gmail.com>

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 class CommentController extends Controller
